9d53d3d32eb355d17e691aaeac8bda1a54330fab
fafazhafira1010 <fyum0001@student.monash.edu>
2026-05-21 01:11:18 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+vegaEmbed('#timeline', 'js/timeline1_vg.json');
+vegaEmbed('#genre_shift', 'js/genre_shift1_vg.json');
+vegaEmbed('#top10_songs', 'js/top10_songs1_vg.json');
+vegaEmbed('#sales', 'js/sales1_vg.json');
+vegaEmbed('#map', 'js/map2_vg.json');
+
